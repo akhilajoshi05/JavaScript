da2d74c0020eec9fd00952de97d99712c302401b
akhilajoshi05 <163162577+akhilajoshi05@users.noreply.github.com>
2025-02-04 17:34:16 +0000

    dts

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,50 @@
+/* datatypes in javascript
+ strings, integers, boolean
+
+ */
+
+// install prettier extension for code indentation 
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3) can not be used in node directly, it can be used in browser not in node js
+
+console.log(3
+   +
+   3); // code readability should be high
+console.log("hiii");
+
+
+/* refer MDN documentationvfor js
+
+https://tc39.es/ecma262/2023/multipage/
+*/
+
+
+let name="abhg"  //string
+let age=12
+let isLoggedIn=false
+let state =" "
+let abc = null
+
+/*
+
+primetive datatypes:
+
+number ==> 2 to power 53
+biint
+string=>""
+boolean =>true/false
+null => standalone value (empty value), it is special type
+undefined
+symbol=> unique 
+
+objects:
+
+*/
+
+
+console.log(typeof age);
+console.log(typeof abc)
+console.log(typeof undefined);
+
+
