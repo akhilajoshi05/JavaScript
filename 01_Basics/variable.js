2b6d80bea1ff13276c9a3531714eb7e7e47737e9
akhilajoshi05 <163162577+akhilajoshi05@users.noreply.github.com>
2025-02-04 17:05:12 +0000

    var

---
+++ 01_Basics/variable.js
@@ -0,0 +1,20 @@
+const accountId= 123456
+let accountGmail="abc@gmail.com"
+var accountPassword ="2121A"
+accountCity= "hyd"
+
+let accountState;                    // declared but not assigned value , thus it is undefined
+// accountId =2      // not allowed in const
+
+accountGmail="asdfghj@sdf.com"
+accountPassword="435253"
+accountCity="bng"
+console.log(accountId);
+console.log([accountGmail, accountPassword,accountCity,accountState]);
+console.table([accountGmail, accountPassword, accountCity, accountState]);
+
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
