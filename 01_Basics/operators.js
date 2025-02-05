da1e1604f8fa767d920fc98343e183f7a3f826f6
akhilajoshi05 <163162577+akhilajoshi05@users.noreply.github.com>
2025-02-05 18:16:43 +0000

    operators

---
+++ 01_Basics/operators.js
@@ -0,0 +1,31 @@
+// operations in JS
+
+let value = 3
+let negValue = -value
+console.log("value:", value);
+console.log("negValue:", negValue);
+
+console.log("sum:",2+2);
+console.log("sub:",2-2);
+console.log("mul:",2*2);
+console.log("div:",2/2);
+console.log("mod:",2%2);
+console.log("power of:",2**2);
+
+
+
+let str1 = "helloo"
+let str2 = " JS"
+let str3 = str1+str2;
+console.log("str3:",str3);
+
+console.log("1" + 2);
+console.log(1 + "3");
+console.log("value2:",1 + 2 + "3");
+console.log("value3:",("1" + 2 + 3));
+
+
+
+
+
+
