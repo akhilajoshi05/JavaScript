b6c3d2ad319a3c7241fc144e84b6fc1ef9aaedcb
akhilajoshi05 <163162577+akhilajoshi05@users.noreply.github.com>
2025-02-05 17:04:58 +0000

    conversion of datatypes

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -1,6 +1,5 @@
 /* datatypes in javascript
  strings, integers, boolean
-
  */
 
 // install prettier extension for code indentation 
@@ -15,10 +14,8 @@ console.log("hiii");
 
 
 /* refer MDN documentationvfor js
-
 https://tc39.es/ecma262/2023/multipage/
 */
-
 
 let name="abhg"  //string
 let age=12
@@ -27,7 +24,6 @@ let state =" "
 let abc = null
 
 /*
-
 primetive datatypes:
 
 number ==> 2 to power 53
@@ -38,13 +34,73 @@ null => standalone value (empty value), it is special type
 undefined
 symbol=> unique 
 
-objects:
-
+objects:{}
 */
-
-
 console.log(typeof age);
 console.log(typeof abc)
 console.log(typeof undefined);
+
+//convertion type 5-2-25
+let score ="33"
+console.log(typeof score);
+console.log(typeof (score));    // as a method
+
+let EmpName = "aj"
+let string1 = Number(EmpName)  ;   
+console.log("string1:",string1);
+
+let num = 33
+console.log(typeof num);
+
+let numb= "uhedw65"
+let valueInNumber = Number(numb)
+console.log(typeof valueInNumber);
+console.log("alphanumaric:",valueInNumber);
+
+let num1 = null
+let valueInNumber1 = Number(num1)
+console.log(typeof valueInNumber1);
+console.log("null:",valueInNumber1);
+
+let num2 = undefined
+let valueInNumber2 = Number(num2)
+console.log(typeof valueInNumber2);
+console.log("undefined:",valueInNumber2);
+
+let num3 = true
+let valueInNumber3 = Number(num3)
+console.log(typeof valueInNumber3);
+console.log("boolean:",valueInNumber3);
+
+
+/**
+ "33"=> 33
+ "33abc"=> NaN
+ true => 1; false => 0
+ */
+
+ let isLoggedIn1 = 1
+ let booleanIsLoggedIn= Boolean(isLoggedIn1)
+ console.log("boolean value:",booleanIsLoggedIn);
+
+ let isLoggedIn2 = ""
+ let booleanIsLoggedIn2= Boolean(isLoggedIn2)
+ console.log("emptyString:",booleanIsLoggedIn2);
+
+ let isLoggedIn3 = "hdiuyiuffy"
+ let booleanIsLoggedIn3= Boolean(isLoggedIn3)
+ console.log("name using string:",booleanIsLoggedIn3);
+ 
+/**
+ * 1=> true; 0 =. false
+ * "" => false
+ * "ajaja" =>true
+ */
+
+
+let sumNumber = 44
+let stringNumber= String(sumNumber);
+console.log("stringNumber",stringNumber);
+console.log("SN",typeof stringNumber);
 
 
