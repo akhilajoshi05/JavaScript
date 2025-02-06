c7144587cb5d120c58d336a6dcb3b9fd2c42edc0
akhilajoshi05 <163162577+akhilajoshi05@users.noreply.github.com>
2025-02-06 11:31:44 +0000

    op

--- 01_Basics/operators.js
+++ 01_Basics/operators.js
@@ -24,6 +24,34 @@ console.log(1 + "3");
 console.log("value2:",1 + 2 + "3");
 console.log("value3:",("1" + 2 + 3));
 
+console.log("expression:", 3 + 5 - 8 * 2 );  /* dont write complex expression like this
+ instead use paranthesis to avoid confusions */
+console.log("exp:", (2+3)*(6/2)-(7));
+
+
+console.log("True:", true);
+console.log("True1:", +true);  // converts to binary 1
+// console.log("True1:", true+);  // error
+
+console.log("True2:", +"");  // converts to binary 0
+
+
+let num1 , num2, num3;
+num1 = num2 = num3 = 5 + 2    // avoid these kind of assignment
+
+let gameCounter1 = 100;
+// gameCounter++;
+++gameCounter1;
+console.log("gameCounter1:",gameCounter1);
+
+let gameCounter2= 100;
+gameCounter2++;
+console.log("gameCounter2:",gameCounter2);
+
+
+/** operators reference for prefix and postfix
+ *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+ */
 
 
 
