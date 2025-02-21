431deb3680d9c627be6c494f55e375a44db7c9f2
Akhila Joshi <163162577+akhilajoshi05@users.noreply.github.com>
2025-02-21 13:04:59 +0000

    if, else

--- 04_controlFlow/if.js
+++ 04_controlFlow/if.js
@@ -1,4 +1,5 @@
 // if 
+
 /**
  * syntax of if 
  *
@@ -6,3 +7,76 @@ if (condition) {
     
 }
  */
+
+/**
+ * <=
+ * >=
+ * ==
+ * !=
+ * ===   it checks the type 
+ */
+
+if (true) {
+    console.log("true");   
+}
+
+if (false) {
+    console.log("false");   
+}
+
+if (2 == 2) {
+    console.log("true: 2 == 2");  
+}
+if (2 === 2) {
+    console.log("strict equal true: 2 === 2");  
+}
+
+// !== negative checking
+if (2 !== 2) {
+    console.log("strict equal true: 2 !== 2");  
+}
+if (3 !== 2) {
+    console.log("strict equal true: 3 !== 2");  
+}
+
+if (2 == "2") {
+    console.log('true: 2 == "2"'); 
+}
+//strict equal
+if (2 === "2") {
+    console.log('true: 2 === "2"'); 
+}
+if ( 3 != 2 ) {
+    console.log('true: 3 != 2'); 
+}
+
+if ( 3 >= 2 ) {
+    console.log('true: 3 >= 2'); 
+}
+
+if ( 3 <= 2 ) {
+    console.log('true: 3 <= 2'); 
+}
+
+const isUserLoggedIn = true;
+const temperature = 40
+
+if (temperature < 50) {
+    console.log("temp less than 50"); 
+}
+console.log(`${temperature} temp is more than 50`);
+
+
+if (temperature < 60) {
+    console.log("temp less than 60"); 
+}else{
+    console.log(`${temperature} temp is more than 60`);
+}
+console.log("after 'if' condition, console log is written then it will execute always");
+
+
+if (temperature === 60) {
+    console.log("temp less than 60"); 
+}else{
+    console.log(`${temperature} temp is more than 60`);
+}
