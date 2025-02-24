ecda20203af3c3684bcb7875da20cdb545a9babd
Akhila Joshi <163162577+akhilajoshi05@users.noreply.github.com>
2025-02-24 07:29:12 +0000

    if, else, nested

--- 04_controlFlow/if.js
+++ 04_controlFlow/if.js
@@ -58,7 +58,7 @@ if ( 3 <= 2 ) {
     console.log('true: 3 <= 2'); 
 }
 
-const isUserLoggedIn = true;
+// const isUserLoggedIn = true;
 const temperature = 40
 
 if (temperature < 50) {
@@ -79,4 +79,40 @@ if (temperature === 60) {
     console.log("temp less than 60"); 
 }else{
     console.log(`${temperature} temp is more than 60`);
+}
+
+const score = 200;
+ if (score > 100) {
+    let power = 'fly'
+    console.log(`user power ${power}`);   
+ }
+
+ // short hand notation 
+ const balance = 1000;
+ if (balance > 500) console.log("test");
+//  if (balance > 500) console.log("test"), console.log("test1");             // it is not a good practice
+ 
+// nested condition 
+if (balance < 500) {
+    console.log(`less than 500`);   
+} else if (balance < 750) {
+    console.log(`less than 750`);   
+} else if (balance < 900) {
+    console.log(`less than 900`);   
+} else {
+    console.log(`less than 1200`); 
+}
+
+const isUserLoggedIn = true ;
+const debitCard = true;
+const isUserLoggedInFromGoogle = false;
+const isUserLoggedInFromEmail = true;
+
+
+if (isUserLoggedIn && debitCard) {
+    console.log(`allow user to buy course`);  
+}
+
+if (isUserLoggedInFromGoogle || isUserLoggedInFromEmail) {
+    console.log(`user logged in`);  
 }
