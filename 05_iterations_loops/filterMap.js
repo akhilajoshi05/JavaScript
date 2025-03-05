7f1febca09280a4dd065a8a9db54b1ab30565d94
Akhila Joshi <163162577+akhilajoshi05@users.noreply.github.com>
2025-03-05 13:22:47 +0000

    forEach

---
+++ 05_iterations_loops/filterMap.js
@@ -0,0 +1,7 @@
+const coding = ['java','python','js','c++']
+
+const values = coding.forEach( (item) => {
+    // console.log(item);
+    return item                         // forEach does not return any values
+})
+console.log(values);
